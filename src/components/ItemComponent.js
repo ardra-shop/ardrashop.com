e8093388460d9785fb98b3aed8c27a853cbcdb1d
Utkarsh Gupta <500075374@stu.upes.ac.in>
2020-09-12 13:00:03 -0700

    Shop Card Working

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, CardBody, CardTitle, UncontrolledCarousel } from 'reactstrap';
+import StarRatings from 'react-star-ratings';
+import { Container, Row, Col, Card, CardBody, CardTitle, UncontrolledCarousel, Button, ButtonGroup } from 'reactstrap';
 
 class ItemComponent extends Component{
     constructor(props){
@@ -11,31 +12,93 @@ class ItemComponent extends Component{
 
     renderCard(item){
         return(
-            <React.Fragment>
+            <li key={item.id}>
                 <Card className='mt-3'>
                     <CardBody>
                         <CardTitle>
                             <h5>
-                                {item.name} <small className='text-success'> {item.cost} </small>
+                                {item.name} <small className='text-success text-sm-right'> {item.cost} </small>
                             </h5>
+                            <hr></hr>
                         </CardTitle>
-                        <Row className='row'>
-                            <Col className='col col-md-4'>
-                                
+                        <Row>
+                            <Col xl="3" lg="4">
+                                <UncontrolledCarousel items={item.photo} />
                             </Col>
-                            <Col className='col col-md-2'>
-                                
+                            <Col xl="1" lg="2" className="d-none d-lg-block">
+                                <Row className="mb-3 mt-lg-3 mt-xl-1">
+                                    <Button size="sm" color="warning" block>
+                                        {item.sold} Sold
+                                    </Button>
+                                </Row>
+                                <Row>
+                                    <Button size="sm" href="#" color="danger" className="mb-3" block>
+                                        <StarRatings rating={item.review}
+                                            starDimension="14px"
+                                            starSpacing="0px"
+                                            starRatedColor="yellow"/>
+                                    </Button>
+                                </Row>
+                                <Row>
+                                    <Button size="sm" href="#" color="success" className="mb-3" block>
+                                        Questions
+                                    </Button>
+                                </Row>
                             </Col>
-                            <Col className='col col-md-4'>
-                                
+                            <Col md="12" className="d-block d-lg-none my-3">
+                                <ButtonGroup className="d-flex">
+                                    <Button color="warning">
+                                        {item.sold} Sold
+                                    </Button>
+                                    <Button href="#" color="danger">
+                                        <StarRatings rating={item.review}
+                                            starDimension="14px"
+                                            starSpacing="0px"
+                                            starRatedColor="yellow"/>
+                                    </Button>
+                                    <Button href="#" color="success">
+                                        Questions
+                                    </Button>
+                                </ButtonGroup>
                             </Col>
-                            <Col className='col col-md-2'>
-                                
+                            <Col xl="6" lg="6">
+                                <h6>
+                                    Description
+                                </h6>
+                                <p>
+                                    {item.description}
+                                </p>
+                            </Col>
+                            <Col xl="2" lg="8" className="offset-lg-2 offset-xl-0">
+                                <div className="d-none d-xl-block">
+                                    <Button size="sm" href="#" color="secondary" className="mb-3" block>
+                                        Add to wishlist
+                                    </Button>
+                                    <Button size="sm" href="#" color="primary" className="mb-3" block>
+                                        Buy now
+                                    </Button>
+                                    <Button size="sm" href="#" color="secondary" className="mb-3" block>
+                                        Add to cart
+                                    </Button>
+                                </div>
+                                <div className="d-xl-none">
+                                    <ButtonGroup className="d-flex" block>
+                                        <Button href="#" color="secondary">
+                                            Add to wishlist
+                                        </Button>
+                                        <Button href="#" color="primary">
+                                            Buy now
+                                        </Button>
+                                        <Button href="#" color="secondary">
+                                            Add to cart
+                                        </Button>
+                                    </ButtonGroup>
+                                </div>
                             </Col>
                         </Row>
                     </CardBody>
                 </Card>
-           </React.Fragment>
+           </li>
         )
     }
 
@@ -43,7 +106,9 @@ class ItemComponent extends Component{
         return(
             <React.Fragment>
                 <Container>
-                    {this.state.items.map((items) => this.renderCard(items))}
+                    <ul className="list-unstyled">
+                        {this.state.items.map((items) => this.renderCard(items))}
+                    </ul>
                 </Container>
             </React.Fragment>
         );
--- src/shared/Items.js
+++ src/shared/Items.js
@@ -4,23 +4,24 @@ export const Items =
         cost : '77.4 M $',
         photo : [
             {
-                src : '/assets/images/AirbusA318/1.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA318/1.jpg'
             },
             {
-                src : '/assets/images/AirbusA318/2.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA318/2.jpg'
             },
             {
-                src : '/assets/images/AirbusA318/3.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA318/3.jpg'
             },
             {
-                src : '/assets/images/AirbusA318/4.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA318/4.jpg'
             },
             {
-                src : '/assets/images/AirbusA318/5.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA318/5.jpg'
             },
         ],
         sold : 32,
         review : 4.1,
+        reviewcount : 21,
         description : 'The Airbus A318 is the smallest airliner in the Airbus A320 family. The A318 carries 107 to 132 passengers and has a maximum range of 5,700 km (3,100 nmi).[3] Final assembly of the aircraft took place in Hamburg, Germany. It is intended primarily for short-range service.'
     },
     {   id : 2,
@@ -28,23 +29,24 @@ export const Items =
         cost : '89.6 M $',
         photo : [
             {
-                src : '/assets/images/AirbusA319/1.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA319/1.jpg'
             },
             {
-                src : '/assets/images/AirbusA319/2.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA319/2.jpg'
             },
             {
-                src : '/assets/images/AirbusA319/3.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA319/3.jpg'
             },
             {
-                src : '/assets/images/AirbusA319/4.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA319/4.jpg'
             },
             {
-                src : '/assets/images/AirbusA319/5.jpg'
+                src : 'https://ardrashopimages.000webhostapp.com/AirbusA319/5.jpg'
             },
         ],
         sold : 117,
         review : 4.5,
+        reviewcount : 75,
         description : 'The Airbus A319 is a member of the Airbus A320 family of short- to medium-range, narrow-body, commercial passenger twin-engine jet airliners manufactured by Airbus. The A319 carries 124 to 156 passengers and has a maximum range of 3,700 nmi (6,900 km; 4,300 mi).'
     }
 ];
