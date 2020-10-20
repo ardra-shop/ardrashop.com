3977f438cd0d93bf31c0535c14c3b9d8740c94d2
Utkarsh Gupta <500075374@stu.upes.ac.in>
2020-10-20 15:03:00 +0530

    adding background image

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import HomeComponent from './components/HomeComponent';
 class App extends Component{
 	render() {
 		return(
-			<BrowserRouter>
-				<NavigationBarComponent/>
-				<Switch>
-					<Route exact path="/ardrashop.com" component={HomeComponent} />
-					<Route exact path="/ardrashop.com/searchresult" component={SearchResultComponent} />
-					<Route exact path="/ardrashop.com/signup" component={SignUpFormComponent} />
-					<Redirect to="/ardrashop.com"/>
-            	</Switch>
-			</BrowserRouter>
+			<div className="backgroundimg">
+				<BrowserRouter>
+					<NavigationBarComponent/>
+					<Switch>
+						<Route exact path="/ardrashop.com" component={HomeComponent} />
+						<Route exact path="/ardrashop.com/searchresult" component={SearchResultComponent} />
+						<Route exact path="/ardrashop.com/signup" component={SignUpFormComponent} />
+						<Redirect to="/ardrashop.com"/>
+					</Switch>
+				</BrowserRouter>
+			</div>
 		);
 	}
 }
